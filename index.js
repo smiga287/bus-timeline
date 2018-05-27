912b744b20fbc957293ff5382e2113e17f052511
Aleksandar Šmigić <smiga287@gmail.com>
2018-05-27 15:18:12 +0200

    Simplified the code A LOT!!!

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
 const puppeteer = require("puppeteer");
-//Add a DB
 
 const HOMEPAGE = "https://www.busevi.com/";
 
 const agenda = async page => {
-  let data = [];
-  let filter = [];
-  const nextPageSelector = ".next";
-  //4 replace with the number of pages
-  for (let i = 1; i <= 4; i++) {
-    if (i > 1) {
-      await page.click(nextPageSelector);
-      await page.waitForSelector(`.row-${i + 5}`);
-      /* 
-        Add a _waitforSomeElement_ cause of the 94 9h bug
-        when switching pages the first hour row from other route is used
-      */
-    }
-    let unfiltered = await page.evaluate(async () => {
-      const rows = Array.from(document.querySelectorAll(".row-hover tr"));
-      return rows.map(row => {
-        const children = Array.from(row.children);
-        const h = children.shift().innerText;
-        return children.map((days, idx) => {
-          const rest = days.innerText.split(" ");
-          return rest.map(m => `${h}:${m}`);
-        });
+  const VIEW_ALL_SELECTOR = "select";
+  const OPTION_VALUE = "-1";
+  await page.select(VIEW_ALL_SELECTOR, OPTION_VALUE);
+  const data = await page.evaluate(() => {
+    const rows = Array.from(document.querySelectorAll(".row-hover tr"));
+    rows.shift(); //Removes the Next arival
+    return rows.map(row => {
+      const children = Array.from(row.children);
+      const h = children.shift().innerText;
+      return children.map((days, idx) => {
+        const rest = days.innerText.split(" ");
+        return rest.map(m => `${h}:${m}`);
       });
     });
-    unfiltered.map(hourArr => {
-      const hour = hourArr[0][0].substr(0, hourArr[0][0].indexOf(":"));
-      if (!filter.includes(hour)) {
-        data.push(hourArr);
-        filter.push(hour);
-      }
-    });
-  }
-  return data;
+  });
+  const getHour = arr => parseInt(arr[0][0].substr(0, arr[0][0].indexOf(":")));
+  const first = getHour(data[0]);
+  return data.filter((hours, idx) => getHour(hours) === first + idx); // Filter out the duplicates created by the other route
 };
 
 const busLink = async (page, num) => {
@@ -54,18 +38,15 @@ const busLink = async (page, num) => {
   return link;
 };
 
-const busTimeline = async (busNum, direction) => {
+const busTimeline = async busNum => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(HOMEPAGE);
   const adress = await busLink(page, busNum);
   await page.goto(adress);
-  // if (!direction) {
-  //   await alterDirection();
-  // }
   const timeline = await agenda(page);
   console.log("timeline", timeline);
   await browser.close();
 };
 
-busTimeline(94, true); // Parametarize
+busTimeline(15);
